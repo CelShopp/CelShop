83be368d38969f01c8b1370efba1937c8d95b8a5
Pranav <prorata2203@gmail.com>
2026-02-24 00:51:56 +0530

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
       <h1 className="text-2xl font-bold mb-4">My Movie Slider</h1>
       
-      {/* Insert the ImageCarousel component */}
+      {/* heyyyy Insert the ImageCarousel component */}
       <ImageCarousel />
 
       {/* Other sections */}
