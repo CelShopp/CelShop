c1c6059dc006000d45a8fc8650af2551942f2fb5
Pranav <prorata2203@gmail.com>
2026-02-24 21:07:08 +0530

    heatmap

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,21 +45,7 @@ export default function RootLayout({
             {children}
           </div>
         </main>
-        <Script
-          id="plerdy-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-    var _protocol="https:"==document.location.protocol?"https://":"http://";
-    _site_hash_code="6f42d72ba604695b0ef24b8224408e79",_suid=73375,plerdyScript=document.createElement("script");
-    plerdyScript.setAttribute("defer",""),plerdyScript.dataset.plerdymainscript="plerdymainscript",
-    plerdyScript.src="https://a.plerdy.com/public/js/click/main.js?v="+Math.random();
-    var plerdymainscript=document.querySelector("[data-plerdymainscript='plerdymainscript']");
-    plerdymainscript&&plerdymainscript.parentNode.removeChild(plerdymainscript);
-    try{document.head.appendChild(plerdyScript)}catch(t){console.log(t,"unable add script tag")}
-    `,
-          }}
-        />
+
       </body>
     </html>
   );
