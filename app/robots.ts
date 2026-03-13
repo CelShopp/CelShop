8d5f363c956aee4641637adc6b4a63775e8733eb
CelShopp <prorata2203@gmail.com>
2026-03-13 20:43:51 +0530

    sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,6 @@ export default function robots() {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://yourdomain.com/sitemap.xml",
+    sitemap: "https://filmyfits.vercel.app/sitemap.xml",
   };
 }
